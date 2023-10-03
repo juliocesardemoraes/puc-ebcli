53132e9fb300a34418951b837ffaab33d3bfde02
julio cesar <jcmcf2012@gmail.com>
2023-10-03 13:38:58 -0300

    feat: add hikes route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
+var express = require("express");
+const hike = require("./hike");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
+
+router.get("/hikes", hike.index);
 
 module.exports = router;
